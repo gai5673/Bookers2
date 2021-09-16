df5c02345ba13bc4d3901bd45961502522b36fe3
EC2 Default User <ec2-user@ip-172-31-21-54.ec2.internal>
2021-09-16 08:00:03 +0000

    [Finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,49 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :baria_user, { only: [:update, :edit] }
 
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+       flash[:notice] = "You have updated user successfully"
+       redirect_to user_path(@user.id)
+    else
+       render :edit
+    end
+  end
+
+  def index
+    @users = User.all
+    @user = current_user
+    @book  = Book.new
+    @books = Book.all
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to user_path(@user)
+    end
+
+  end
+
+  private
+  def user_params
+   params.require(:user).permit(:name, :introduction , :profile_image)
+  end
+
+  def baria_user
+    unless params[:id].to_i == current_user.id
+      redirect_to user_path(current_user)
+    end
   end
 
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '372876c853d3a0edcfb6936bfc983333471eb932f6f6b036d430f22c4110c135ba7af664cfa75d6f33d7bce38b23dcb6db02f95b8385469b85186b8f9b86d7a1'
